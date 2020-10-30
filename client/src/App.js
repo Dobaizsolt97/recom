6f17a8a44b3cdd42c928f781400e326fe4cedb98
unknown <dobaizsolt97@gmail.com>
2020-10-30 19:02:50 +0200

    presentational style

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Container } from "react-bootstrap";
+import { Route } from "react-router-dom";
+
 const App = () => {
   return (
     <>
-      <h1>working starter</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
