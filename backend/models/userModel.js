bc204f6f54cf9635cc93213bd7a884124bc9dd33
unknown <dobaizsolt97@gmail.com>
2020-10-31 14:17:33 +0200

    data seeder and routes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const userSchema = mongoose.Schema(
   {
-    user: { type: String, required: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false },
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
 connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(
@@ -16,13 +19,12 @@ app.listen(
   )
 );
 
+app.use("/api/products", productRoutes);
+
 app.get("/", (req, res) => {
   res.send("api is running");
 });
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
+app.use(notFound);
+
+app.use(errorHandler);
