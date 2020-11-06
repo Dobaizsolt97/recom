63895879b9a8a82923fae7a40268e97287948173
Zsolt Dobai <dobaizsolt97@gmail.com>
2020-11-06 11:54:19 +0200

    user constants,actions,reducers

--- client/src/store.js
+++ client/src/store.js
@@ -6,17 +6,27 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
+
+// The root reducer is the combination of all the reducers
 const rootReducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 const middleware = [thunk];
+
+// !This is setting the initial state based on what we have in local storrage
 const cartItemsFromStorrage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
+const userInfoFromStorrage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 const initialState = {
   cart: { cartItems: cartItemsFromStorrage },
+  userLogin: { userInfo: userInfoFromStorrage },
 };
 
 const store = createStore(
